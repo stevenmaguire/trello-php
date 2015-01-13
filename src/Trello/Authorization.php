f4bc6d708f30add010aeb7c3cdccf6d406ab399a
Steven Maguire <steven@stevenmaguire.com>
2015-01-13 14:18:28 -0600

    adding early automatic OAuth flow

--- src/Trello/Authorization.php
+++ src/Trello/Authorization.php
@@ -40,6 +40,49 @@ abstract class Trello_Authorization extends Trello
     }
 
     /**
+     * Authorize application
+     *
+     * First part of OAuth 1.0 authentication is retrieving temporary
+     * credentials. These identify you as a client to the server. Store the
+     * credentials in the session. Second part of OAuth 1.0 authentication is
+     * to redirect the resource owner to the login screen on the server.
+     *
+     * Redirects to authorization url to fetch authorization
+     */
+    public static function authorize($expiration = null)
+    {
+        $server = self::getOAuthServer($expiration);
+        $temporaryCredentials = $server->getTemporaryCredentials();
+        $_SESSION['temporary_credentials'] = serialize($temporaryCredentials);
+        session_write_close();
+        $server->authorize($temporaryCredentials);
+    }
+
+    /**
+     * Verify and fetch token
+     *
+     * Retrieve the temporary credentials from step 2. Third and final part to
+     * OAuth 1.0 authentication is to retrieve token credentials (formally
+     * known as access tokens in earlier OAuth 1.0 specs). Now, we'll store
+     * the token credentials and discard the temporary ones - they're
+     * irrelevant at this stage.
+     *
+     * @return string
+     */
+    public static function getToken($oauth_token, $oauth_verifier)
+    {
+        $server = self::getOAuthServer($expiration);
+        $temporaryCredentials = unserialize($_SESSION['temporary_credentials']);
+        $tokenCredentials = $server->getTokenCredentials(
+            $temporaryCredentials,
+            $oauth_token, $oauth_verifier
+        );
+        unset($_SESSION['temporary_credentials']);
+        session_write_close();
+        return serialize($tokenCredentials);
+    }
+
+    /**
      * Parse expiration value for fixed Trello values
      *
      * @param  string $expiration
@@ -64,5 +107,25 @@ abstract class Trello_Authorization extends Trello
     protected static function getBasePath()
     {
         return 'https://trello.com'.Trello_Configuration::versionPath().self::$base_path;
+    }
+
+    /**
+     * Get OAuth server implementation
+     *
+     * @param  string $expiration
+     *
+     * @return Stevenmaguire\OAuth2\Client\Server\Trello
+     */
+    protected static function getOAuthServer($expiration = null)
+    {
+        session_start();
+        return new \Stevenmaguire\OAuth2\Client\Server\Trello(array(
+            'identifier' => Trello_Configuration::key(),
+            'secret' => Trello_Configuration::secret(),
+            'callback_uri' => 'http://your-callback-uri/',
+            'name' => Trello_Configuration::applicationName(),
+            'expiration' => self::parseExpiration($expiration),
+            'scope' => static::$scope
+        ));
     }
 }
