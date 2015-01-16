e3d9361c53272eaa2baa5e9e1364d80105a8f36a
Steven Maguire <steven@stevenmaguire.com>
2015-01-16 15:50:06 -0600

    adding card list

---
+++ src/Trello/CardList.php
@@ -0,0 +1,209 @@
+<?php namespace Trello;
+
+/**
+ * Trello list
+ *
+ * @package    Trello
+ * @subpackage Models
+ * @copyright  2014 Steven Maguire
+ *
+ * @property-read string $id
+ * @property-read string $name
+ * @property-read array $cards
+ * @property-read array $card_fields
+ * @property-read array $fields
+ */
+class CardList extends Model
+{
+    /**
+     * List id
+     *
+     * @property string $id
+     */
+    protected $id;
+
+    /**
+     * List name
+     *
+     * @property string $name
+     */
+    protected $name;
+
+    /**
+     * List cards
+     *
+     * @property array $cards
+     */
+    protected $cards;
+
+    /**
+     * List card fields
+     *
+     * @property array $card_fields
+     */
+    protected $card_fields;
+
+    /**
+     * List fields
+     *
+     * @property array $fields
+     */
+    protected $fields;
+
+    /**
+     * Lists base path
+     *
+     * @var string
+     */
+    protected static $base_path = 'lists';
+
+    /**
+     * fetch a list
+     *
+     * @param  string|array $list_id List id to fetch
+     *
+     * @return List|Collection  List model(s)
+     * @throws Exception_ValidationsFailed
+     */
+    public static function fetch($list_id = null)
+    {
+        if (empty($list_id)) {
+            throw new \Trello\Exception\ValidationsFailed(
+                'attempted to fetch list without id; it\'s gotta have an id'
+            );
+        }
+
+        return static::doFetch($list_id);
+    }
+
+    /**
+     * create a new list
+     *
+     * @param  array $attributes Board attributes to set
+     *
+     * @return Board  Newly minted trello board
+     * @throws Exception_ValidationsFailed
+     */
+    public static function create($attributes = [])
+    {
+        self::parseAttributes($attributes);
+        return static::doCreate(static::getBasePath(), $attributes);
+    }
+
+    /**
+     * Parse attributes
+     *
+     * @param  array $attributes
+     */
+    public static function parseAttributes(&$attributes)
+    {
+        $defaults = ['name' => null, 'idBoard' => null];
+        $attributes = array_merge($defaults, $attributes);
+
+        self::parseName($attributes);
+        self::parseBoardId($attributes);
+        self::parsePosition($attributes);
+    }
+
+    /**
+     * Parse attributes for name
+     *
+     * @param  array $attributes
+     */
+    private static function parseName(&$attributes)
+    {
+        if (empty($attributes['name'])) {
+            throw new \Trello\Exception\ValidationsFailed(
+                'attempted to add list to board without list name; it\'s gotta have a name'
+            );
+        }
+    }
+
+    /**
+     * Parse attributes for board id
+     *
+     * @param  array $attributes
+     */
+    private static function parseBoardId(&$attributes)
+    {
+        if (empty($attributes['idBoard'])) {
+            throw new \Trello\Exception\ValidationsFailed(
+                'attempted to create list without board; it\'s gotta have a board'
+            );
+        }
+    }
+
+    /**
+     * Parse attributes for position
+     *
+     * @param  array $attributes
+     */
+    private static function parsePosition(&$attributes)
+    {
+        if (isset($attributes['position'])) {
+            $positions = ['top','bottom','\d{'.strlen($attributes['position']).'}'];
+
+            $is_match = Util::matches(
+                $positions,
+                strtolower($attributes['position'])
+            );
+            if (!$is_match) {
+                unset($attributes['position']);
+            }
+        }
+    }
+
+    /**
+     * Get list ids from list of lists
+     *
+     * @param  stdClass|null $lists List of lists
+     *
+     * @return array List of list ids
+     */
+    public static function getListIds($lists = [])
+    {
+        return Util::getItemProperties($lists, 'id');
+    }
+
+    /**
+     * Get card models for list
+     *
+     * @return Collection|Card Card model(s)
+     */
+    public function getCards()
+    {
+        $ids = [];
+        $cards = static::get('/list/'.$this->id.'/cards');
+        if (is_array($cards)) {
+            foreach ($cards as $card) {
+                $ids[] = $card->id;
+            }
+        }
+        return Card::fetch($ids);
+    }
+
+    /**
+     * create a new card
+     *
+     * @param  array $attributes Card attributes to set
+     *
+     * @return Card  Newly minted trello card
+     */
+    public function createCard($attributes = [])
+    {
+        $attributes['idList'] = $this->id;
+        return Card::create($attributes);
+    }
+
+    /**
+     * add a card to the list
+     *
+     * @param  Card $card Card to add
+     *
+     * @return Card Updated card
+     */
+    public function addCard(Card $card)
+    {
+        return $card->updateList($this);
+    }
+}
---
+++ tests/integration/CardList_Test.php
@@ -0,0 +1,106 @@
+<?php namespace Trello\Tests\Integration;
+
+use \ReflectionClass;
+use Trello\CardList;
+
+class CardList_Test extends IntegrationTestCase
+{
+    public function test_It_Can_Fetch_A_List_When_Id_Provided()
+    {
+        $list = $this->createTestList();
+
+        $result = CardList::fetch($list->id);
+
+        $this->assertInstanceOf('Trello\CardList', $result);
+        $this->assertEquals($list->id, $result->id);
+    }
+
+    public function test_It_Can_Fetch_A_Collection_Of_Lists_When_Ids_Provided()
+    {
+        $list1 = $this->createTestList();
+        $list2 = $this->createTestList(true);
+
+        $result = CardList::fetch([$list1->id, $list2->id]);
+
+        $this->assertInstanceOf('Trello\Collection', $result);
+        $this->assertEquals($list1->id, $result[0]->id);
+        $this->assertEquals($list2->id, $result[1]->id);
+    }
+
+    /**
+     * @expectedException Trello\Exception\ValidationsFailed
+     */
+    public function test_It_Can_Not_Fetch_A_List_When_Id_Not_Provided()
+    {
+        $list = CardList::fetch();
+    }
+
+    public function test_It_Can_Create_A_List_When_Name_And_Board_Id_Provided()
+    {
+        $board = $this->createTestBoard();
+        $attributes = ['name' => 'test', 'idBoard' => $board->id];
+
+        $list = CardList::create($attributes);
+
+        $this->assertInstanceOf('Trello\CardList', $list);
+        $this->assertEquals($board->id, $list->idBoard);
+    }
+
+    /**
+     * @expectedException Trello\Exception\ValidationsFailed
+     */
+    public function test_It_Can_Not_Create_A_List_When_Name_Not_Provided()
+    {
+        $list = CardList::create();
+    }
+
+    /**
+     * @expectedException Trello\Exception\ValidationsFailed
+     */
+    public function test_It_Can_Not_Create_A_List_When_Board_Id_Not_Provided()
+    {
+        $attributes = ['name' => 'test'];
+
+        $list = CardList::create($attributes);
+    }
+
+    public function test_It_Can_Fetch_Collection_Of_Child_Cards()
+    {
+        $attributes = ['name' => 'test card'];
+        $list = $this->createTestList();
+        $card = $list->createCard($attributes);
+
+        $result = $list->getCards();
+
+        $this->assertInstanceOf('Trello\Collection', $result);
+    }
+
+    public function test_It_Can_Add_A_Card_When_Card_Model_Provided()
+    {
+        $list = $this->createTestList();
+        $card = $this->createTestCard();
+
+        $result = $list->addCard($card);
+
+        $this->assertInstanceOf('Trello\Card', $result);
+    }
+
+    public function test_It_Can_Get_Individual_Fields_When_Property_Does_Exist()
+    {
+        $list = $this->createTestList();
+        $ref_list = new ReflectionClass($list);
+        $fields = $ref_list->getProperties();
+
+        foreach ($fields as $field) {
+            if (!$field->isStatic()) {
+                $response = $list->getField($field->name, true);
+            }
+        }
+    }
+
+    public function test_It_Can_Get_Individual_Fields_When_Property_Does_Not_Exist()
+    {
+        $list = $this->createTestList();
+        $response = $list->getField('foo', true);
+    }
+}
